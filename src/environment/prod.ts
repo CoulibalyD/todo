c035ff5285c93d8f4cba2e9aea70238548ce91d9
CoulibalyD <dramane.coulibalyd636@gmail.com>
2025-07-24 19:42:21 +0000

    _ok

--- src/environment/prod.ts
+++ src/environment/prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   apiUrl: 'https://todo-back-a49q.onrender.com'
-   // apiUrl: 'http://localhost:8080/api/tasks',
+   apiUrl: 'https://todo-back-a49q.onrender.com/api/tasks'
+   //apiUrl: 'http://localhost:8080/api/tasks',
  };
