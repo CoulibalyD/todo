51cb4a15cf4c7faefa56966cc1f8485e83cd7a81
CoulibalyD <dramane.coulibalyd636@gmail.com>
2025-08-05 19:45:43 +0000

    voilà...

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,40 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
-import {environment} from '../../environment/prod';
-import {User} from '../model/user.model';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { environment } from '../../environment/prod';
+import { User } from '../model/user.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private baseUrl = environment.apiUrl + '/auth';
-  //private apiUrl = 'http://localhost:8080/api/auth';
+
+  private authStatusSubject = new BehaviorSubject<boolean>(this.isTokenAvailable());
+  authStatus$ = this.authStatusSubject.asObservable(); // expose l'état observable
 
   constructor(private http: HttpClient) {}
 
+  private isTokenAvailable(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
   login(credentials: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/login`, credentials).pipe(
-      tap((res: any) => localStorage.setItem('token', res.token))
+      tap((res: any) => {
+        localStorage.setItem('token', res.token);
+        this.authStatusSubject.next(true); // notif les abonnés que l'user est connecté
+      })
     );
   }
 
-  register(data: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/register`, data);
+  logout(): void {
+    localStorage.removeItem('token');
+    this.authStatusSubject.next(false); // notif les abonnés que l'user est déconnecté
   }
 
   getToken(): string | null {
     return localStorage.getItem('token');
   }
 
-  logout(): void {
-    localStorage.removeItem('token');
-  }
-
   isLoggedIn(): boolean {
-    return !!this.getToken();
+    return this.isTokenAvailable();
   }
 
   getCurrentUser(): Observable<User> {
     return this.http.get<User>(`${this.baseUrl}/me`);
   }
 
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, data);
+  }
 }
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,30 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {NgIf, NgOptimizedImage} from '@angular/common';
+import { NgIf, NgClass, NgOptimizedImage } from '@angular/common';
+import { Subscription } from 'rxjs';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, NgIf, NgOptimizedImage],
+  imports: [RouterModule, NgIf, NgOptimizedImage, NgClass],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
+  isMenuOpen = false;
+  private authSub!: Subscription;
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    this.checkAuth();
-  }
-
-  checkAuth(): void {
-    const token = localStorage.getItem('token');
-    this.isAuthenticated = !!token;
+    this.authSub = this.authService.authStatus$.subscribe(status => {
+      this.isAuthenticated = status;
+    });
   }
 
   logout(): void {
-    localStorage.removeItem('token');
-    this.isAuthenticated = false;
+    this.authService.logout();
+    this.isMenuOpen = false;
     window.location.href = '/login';
   }
-}
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  ngOnDestroy(): void {
+    this.authSub?.unsubscribe();
+  }
+}
